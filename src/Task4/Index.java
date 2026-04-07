b7b6b2b4a314d7393c3b934cd889666c30f42bfa
Norman Panlaqui Ablaza <namronazalba@gmail.com>
2026-04-07 16:02:21 +0800

    add scanner

--- src/Task4/Index.java
+++ src/Task4/Index.java
@@ -1,15 +1,34 @@
 package src.Task4;
-
+import java.util.Scanner;
 public class Index {
 
 
     public static void main(String[] args) {
-        String input = "testing"; 
-        StringBuilder reversed = new StringBuilder(input).reverse();
-        if (input.equals(reversed.toString())) {
-            System.out.println("The input string is a palindrome.");
-        } else {
-            System.out.println("The input string is not a palindrome.");
+        Scanner Scanner = new Scanner(System.in);
+        System.out.print("Enter a string or 'exit' to quit: ");
+        while (true) {
+
+            String input = Scanner.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Program terminated.");
+                break;
+            }
+            try {
+                StringBuilder reversed = new StringBuilder(input).reverse();
+                if (input.equals(reversed.toString())) {
+                    System.out.println("The string is a palindrome.");
+                    System.out.println("Enter another string or 'exit' to quit: ");
+                } else {
+                    System.out.println("The string is not a palindrome.");
+                    System.out.println("Enter another string or 'exit' to quit: ");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a string or 'exit' to quit.");
+            }
+           
+
         }
+         Scanner.close();
+
     }
 }
