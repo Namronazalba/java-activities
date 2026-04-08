0024685c549223785bd83d4b441fc02014ce95c4
Norman Panlaqui Ablaza <namronazalba@gmail.com>
2026-04-08 13:34:38 +0800

    submit String Methods

--- src/StringMethods/Main.java
+++ src/StringMethods/Main.java
@@ -1,7 +1,31 @@
 package src.StringMethods;
 
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input String or 'exit' to end program");
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Program end.");
+                break;
+            }
+
+            int inputLength = input.length();
+            String uppercase = input.toUpperCase();
+            String lowercase = input.toLowerCase();
+            char firstChar = input.charAt(0);
+            int indexLength = inputLength-1;
+            char lastChar = input.charAt(indexLength);
+            System.out.println(inputLength+", "+uppercase+", "+lowercase+", "+firstChar+", "+lastChar);
+            
+            if(input.length() >= 5){
+                String substr = input.substring(1, 5);
+                System.out.println(substr);
+            }
+        }
+        scan.close();
     }
 }
