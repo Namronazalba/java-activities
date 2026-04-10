9cdd993de6de96e261348b8cc9d1a74dd9834f0f
Norman Panlaqui Ablaza <namronazalba@gmail.com>
2026-04-10 13:26:15 +0800

    submit check vowels and consonants

--- src/CheckVowels/Main.java
+++ src/CheckVowels/Main.java
@@ -1,8 +1,44 @@
 package src.CheckVowels;
 
+import java.util.Scanner;
 
 class Main{
+    static String checkVowels(char input){
+
+        switch (Character.toLowerCase(input)) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return "vowel";    
+            default:
+                return "Consonant";
+
+        }
+  
+    }
     public static void main(String[] args) {
-        
+        System.out.println("Input char");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String inputString = sc.nextLine();
+            char inputChar = inputString.charAt(0);
+
+            if (inputString.equalsIgnoreCase("exit")) {
+                System.out.println("Program end.");
+                break;
+            }
+            if (inputString.length() != 1) {
+                System.out.println("Input char or 'exit' to end the program");
+                continue;
+            }
+
+            System.out.println(checkVowels(inputChar));
+
+        }
+        sc.close();
+
     }
 }
