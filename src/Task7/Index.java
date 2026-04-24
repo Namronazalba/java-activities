8f1f2928a9bde9832cfe9f2c1d348085c83c1b38
Norman Panlaqui Ablaza <namronazalba@gmail.com>
2026-04-24 16:00:43 +0800

    update task 7

--- src/Task7/Index.java
+++ src/Task7/Index.java
@@ -6,24 +6,37 @@ public class Index {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Index obj = new Index();
 
-        while (true) { 
+        while (true) {
+
+            System.out.println("Input 'exit' to end OR press Enter to continue:");
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Program end");
+                break;
+            }
+
             System.out.println("input two numbers to perform 4 basic arithmetic operations");
-            
-            System.out.println("input num 1");
-            int num1 = sc.nextInt();
-            System.out.println("Input num2");
-            int num2 = sc.nextInt();
+
+            System.out.print("input num 1: ");
+            int num1 = Integer.parseInt(sc.nextLine());
+
+            System.out.print("input num 2: ");
+            int num2 = Integer.parseInt(sc.nextLine());
+
             System.out.println("--------------------------------");
-            Index obj = new Index();
-            
+
             obj.addition(num1, num2);
             obj.subtraction(num1, num2);
             obj.multiplication(num1, num2);
             obj.division(num1, num2);
+
             System.out.println("==============================================");
         }
 
+        sc.close();
     }
 
     public void addition(int  x, int y){
