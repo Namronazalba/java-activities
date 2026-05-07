fc537b868c88e9173cf64518c5dcd50c64872ad2
Norman <namronazakba@gmail.com>
2026-05-07 12:14:06 +0800

    submit task5

---
+++ src/Task5/Main.java
@@ -0,0 +1,36 @@
+package Task5;
+
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        // Check if all are equal
+        if (a == b && b == c) {
+            System.out.println("All numbers are equal");
+        }
+        // Check largest number
+        else if (a >= b && a >= c) {
+            System.out.println("Largest number is: " + a);
+        }
+        else if (b >= a && b >= c) {
+            System.out.println("Largest number is: " + b);
+        }
+        else {
+            System.out.println("Largest number is: " + c);
+        }
+
+        sc.close();
+    }
+}
